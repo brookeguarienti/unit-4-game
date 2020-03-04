e8c1bda8165e837eb3307ef822dea1355593d721
Brooke G <brooke.a.guarienti@gmail.com>
2020-03-04 12:28:11 -0600

    added to game start to allow wins, losses, and counter to appear as 0 at beginning of game. added counter = 0 into game start. added winLogic function to each click button function. updated html to add divs and rearrange IDs to wins/losses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,190 +1,115 @@
+$(document).ready(function(){
 
-
-// number that computer randomly selects
+// variable that holds randomly selected computer number
 var targetNumber = 0;
+// variable that holds wins
 var wins = 0;
+// variable that holds losses
 var losses = 0;
+// variable that holds user guesses
 var counter = 0;
-var numberOptions = [];
-var img = [
+// //variable that holds array of number options
+// var numberOptions = [];
+// variable that holds each crystal item with name and value 
+    // value set to 0 so it can hold randomly selected computer number
+var img = {
+    blueCrystal:
     {
-        name: "Blue Crystal",
+        name: "blueCrystal",
         value: 0,
     },
+    clearCrystal:
     {
-        name: "Clear Crystal",
+        name: "clearCrystal",
         value: 0,
     },
+    redCrystal:
     {
-        name: "Red Crystal",
+        name: "redCrystal",
         value: 0,
     },
+    purpleCrystal:
     {
-        name: "Purple Crystal",
+        name: "purpleCrystal",
         value: 0,
     }
-];
+};
 
-// variable resetGame equals the function and holds the variables that will reset the game 
-var resetGame = function() {
-var targetNumber = 0;
-var wins = 0;
-var losses = 0;
-var counter = 0;
-var numberOptions = [];
-var img = [
-    {
-        name: "Blue Crystal",
-        value: 0,
-    },
-    {
-        name: "Clear Crystal",
-        value: 0,
-    },
-    {
-        name: "Red Crystal",
-        value: 0,
-    },
-    {
-        name: "Purple Crystal",
-        value: 0,
-    }
-];
 
-}
-
-min = 20;
-max = 120;
+function gameStart(){
 
 // chooses a random number between 19 and 120 to appear as our targetNumber
-var targetNumber = Math.floor(Math.random() * (max - min + 1) ) + min;
+targetNumber = Math.floor(Math.random() * 102 + 19)
 
 // puts the computer chosen number from var targetNumber to class computer-number displayed on screen 
 $(".computer-number").text(targetNumber);
 
-// for loop to loop through the numberOptions array 
-for (let i = 0; i < numberOptions.length; i++) {
-    img.attr("data-crystalvalue", numberOptions[i]);
-// selects the img     
-    // var img = $("<img>");
-    // img.attr("data-crystalvalue", numberOptions[i]);
-    // $("#crystals").append(img);
+$(".counter").text(counter);
+
+$("#wins").text(wins);
+
+$("#losses").text(losses);
+
+counter = 0;
+
+// grabs the crystal value from img variable and applies a random number between 1 and 12
+img.blueCrystal.value = Math.floor(Math.random() * (12 ) + 1);
+img.clearCrystal.value = Math.floor(Math.random() * (12 ) + 1);
+img.redCrystal.value = Math.floor(Math.random() * (12 ) + 1);
+img.purpleCrystal.value = Math.floor(Math.random() * (12 ) + 1);
 }
 
-// adds the event listener "click" to each crystal img, and then assigns a random number to it
-$(".crystals").on("click", function() {
-    min = 1;
-    max = 12;
-    var crystalValue = Math.floor(Math.random() * (max - min + 1) ) + min;
-    console.log(crystalValue);
-    
-    crystalValue = parseInt(crystalValue);
-
-    // add crystalValue to user's "counter", a global variable
-    // with every click from each crystal, adds to the global counter// same as counter = counter + crystalValue
-    counter += crystalValue; 
-
-    // all of the same game win-lose logic applies
-    $(".counter").text(counter);
-
-    // win logic 
-    if (counter === targetNumber){
-        wins++;
-       console.log(wins);
-       resetGame();
-       console.log(resetGame());
-       
-        $("#wins").text("wins: " + wins);
-    } else if (counter >= targetNumber) {
+// win logic 
+function winLogic(){
+    if (counter > targetNumber) {
         losses++;
-        console.log(losses);
-        resetGame();
-        console.log(resetGame());
-        
-        $("#losses").text("losses: " + losses);
-    } else{
-        return false;
+        console.log("losses: " + losses);
+    
+        $("#losses").text(losses);
+    
+        gameStart();
+    } 
+    else if (counter === targetNumber){
+        wins++;
+       console.log("wins: " + wins);
+       
+        $("#wins").text(wins);
+    
+        gameStart();
+    } 
     }
+
+// call the function gameStart
+gameStart();
+
+// adds the event listener "click" to each crystal img
+// then pull the chosen value and add it to the user counter
+$("#blueCrystal").on("click", function() {
+  counter += img.blueCrystal.value;
+   console.log(img.blueCrystal.value);
+   $(".counter").text(counter);
+   console.log("counter:" + counter);
+   winLogic();
 });
-
-
-
-
-// $(document).ready(function() {
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // Create a for-loop to iterate through the img array.
-// for(var i=0; i < numberOptions.length; i++){
-
-// // Create a variable named "imgBtn" equal to $("<button>");
-//     var imgBtn = $('<button>');
-//     console.log(imgBtn)
-
-// // Whenever the button is clicked...
-// $("#crystals").on("click", function(){
-//     alert("i've been clicked");
-// });
-
-// // create a variable that sets a random number selection into an array 
-//     function getRandomNum(min, max){
-//        return Math.floor(Math.random() * (max - min + 1) ) + min;
-//     } 
-    
-//     $(".computer-number").text(numberOptions)
-//         console.log(numberOptions);
-//    var numberOptions = getRandomNum
-//         console.log(numberOptions)
-
-// // assign a random number to each imgBtn
-//     imgBtn.attr("data-img", numberOptions[i]);
-//     console.log()
-
-// };
-
-
-// $(".crystal-image").on("click", function() {
-//     var crystalValue = ($(this).attr("data-img"));
-//     crystalValue = parseInt(crystalValue);
-//     // add crystalValue to user's "counter", a global variable
-//     // with every click from each crystal, adds to the global counter// same as counter = counter + crystalValue
-//     counter += crystalValue;
-
-//     // all of the same game win-lose logic applies
-//     alert("New Score: " + counter);
-
-//     if (counter === targetRandomNum){
-//         alert("You win!");
-//     } else if (counter >= targetRandomNum){
-//         alert("You lose!");
-//     }
-
-
-    
-// });
-
-
-// // when game starts, computer selects a random number and displays on screen
-// function getRandomNum(min, max) {
-//     return Math.floor(Math.random() * (max - min) ) + min;
-// }
-//  // create a for loop that assigns a random number to each crystal img each time the game restarts
-//  for (var i = 0; i < numberOptions.length; i++) {
-
-//   }
-
-
-// computer will randomly assign a number to each crystal image
-// user will click on one of the crystals, 
-// > which will increase their counter until they lose or win
-// 
+$("#clearCrystal").on("click", function() {
+    counter += img.clearCrystal.value;
+   console.log(img.clearCrystal.value);
+   $(".counter").text(counter);
+   console.log("counter:" + counter);
+   winLogic();
+});
+$("#redCrystal").on("click", function() {
+    counter += img.redCrystal.value;
+   console.log(img.redCrystal.value);
+   $(".counter").text(counter);
+   console.log("counter:" + counter);
+   winLogic();
+});
+$("#purpleCrystal").on("click", function() {
+    counter += img.purpleCrystal.value;
+   console.log(img.purpleCrystal.value);
+   $(".counter").text(counter);
+   console.log("counter:" + counter);
+   winLogic();
+});
+})
